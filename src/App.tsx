1696f2823b378e1003fee0599479a147fc2347b4
Julian Rosse <julian@helixbass.net>
2020-06-26 19:15:10 -0400

    center square

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React, {FC} from 'react'
-import {flowMax, addDisplayName, addWrapper, addProps} from 'ad-hok'
+import {
+  flowMax,
+  addDisplayName,
+  addWrapper,
+  addProps,
+  addStateHandlers,
+} from 'ad-hok'
 import {addLayoutEffectOnMount} from 'ad-hok-utils'
 import gsap from 'gsap'
 
@@ -16,32 +22,50 @@ const SQUARE_WIDTH = 240
 const SQUARE_STROKE_WIDTH = 3.3
 const SQUARE_OVERLAP = SQUARE_WIDTH * 0.2
 
+const SQUARE_ENTRANCE_DURATION = 0.5
+
 interface Point {
   x: number
   y: number
 }
 
+type AddSquarePathType = <TProps extends {center: Point}>(
+  props: TProps,
+) => TProps & {path: string}
+
+const addSquarePath: AddSquarePathType = addProps(
+  ({center: {x, y}}) => ({
+    path: `M ${x - SQUARE_WIDTH / 2} ${y} L ${x} ${y - SQUARE_WIDTH / 2} L ${
+      x + SQUARE_WIDTH / 2
+    } ${y} L ${x} ${y + SQUARE_WIDTH / 2} Z`,
+  }),
+  ['center'],
+)
+
 interface SquareProps {
   center: Point
   index: number
+  onEntranceComplete: () => void
 }
 
 const Square: FC<SquareProps> = flowMax(
   addDisplayName('Square'),
   addRefs,
-  addLayoutEffectOnMount(({index, refs}) => () => {
+  addLayoutEffectOnMount(({index, refs, onEntranceComplete}) => () => {
     const {path} = refs
-    const DURATION = 0.5
     if (index === 0) {
       gsap.from(path, {
-        duration: DURATION,
+        duration: SQUARE_ENTRANCE_DURATION,
         rotation: radiansToDegrees(-PI),
         x: -SQUARE_WIDTH * 3,
+        onUpdate: function () {
+          if (this.progress() > 0.3) onEntranceComplete()
+        },
       })
     }
     if (index === 1) {
       gsap.from(path, {
-        duration: DURATION - 0.1,
+        duration: SQUARE_ENTRANCE_DURATION - 0.1,
         y: SQUARE_WIDTH / 2 - SQUARE_OVERLAP / 2,
         ease: 'power1.inOut',
         delay: 0.1,
@@ -49,21 +73,21 @@ const Square: FC<SquareProps> = flowMax(
     }
     if (index === 2) {
       gsap.from(path, {
-        duration: DURATION,
+        duration: SQUARE_ENTRANCE_DURATION,
         rotation: radiansToDegrees(PI),
         x: SQUARE_WIDTH * 3,
       })
     }
     if (index === 3) {
       gsap.from(path, {
-        duration: DURATION,
+        duration: SQUARE_ENTRANCE_DURATION,
         rotation: radiansToDegrees(PI),
         x: -SQUARE_WIDTH * 3,
       })
     }
     if (index === 4) {
       gsap.from(path, {
-        duration: DURATION - 0.1,
+        duration: SQUARE_ENTRANCE_DURATION - 0.1,
         y: -(SQUARE_WIDTH / 2 - SQUARE_OVERLAP / 2),
         ease: 'power1.inOut',
         delay: 0.1,
@@ -71,20 +95,13 @@ const Square: FC<SquareProps> = flowMax(
     }
     if (index === 5) {
       gsap.from(path, {
-        duration: DURATION,
+        duration: SQUARE_ENTRANCE_DURATION,
         rotation: radiansToDegrees(-PI),
         x: SQUARE_WIDTH * 3,
       })
     }
   }),
-  addProps(
-    ({center: {x, y}}) => ({
-      path: `M ${x - SQUARE_WIDTH / 2} ${y} L ${x} ${y - SQUARE_WIDTH / 2} L ${
-        x + SQUARE_WIDTH / 2
-      } ${y} L ${x} ${y + SQUARE_WIDTH / 2} Z`,
-    }),
-    ['center'],
-  ),
+  addSquarePath,
   ({path, setRef, center}) => (
     <path
       ref={setRef('path')}
@@ -92,6 +109,38 @@ const Square: FC<SquareProps> = flowMax(
       stroke={colors.white}
       strokeWidth={SQUARE_STROKE_WIDTH}
       fill="none"
+      data-svg-origin={`${center.x} ${center.y}`}
+    />
+  ),
+)
+
+const CenterSquare: FC = flowMax(
+  addDisplayName('CenterSquare'),
+  addProps({
+    center: {
+      x: WIDTH / 2,
+      y: HEIGHT / 2,
+    },
+  }),
+  addSquarePath,
+  addRefs,
+  addRenderingDelay(200),
+  addLayoutEffectOnMount(({refs}) => () => {
+    const {path} = refs
+    gsap.from(path, {
+      duration: 0.5,
+      opacity: 0,
+      scale: 0.6,
+      ease: 'back.out(1.5)',
+    })
+  }),
+  ({path, center, setRef}) => (
+    <path
+      ref={setRef('path')}
+      d={path}
+      stroke={colors.white}
+      strokeWidth={SQUARE_STROKE_WIDTH}
+      fill={colors.white}
       data-svg-origin={`${center.x} ${center.y}`}
     />
   ),
@@ -140,11 +189,27 @@ const App: FC = flowMax(
       },
     ],
   }),
-  ({squareCenters}) => (
+  addStateHandlers(
+    {
+      isSquareEntranceComplete: false,
+    },
+    {
+      onSquareEntranceComplete: () => () => ({
+        isSquareEntranceComplete: true,
+      }),
+    },
+  ),
+  ({squareCenters, onSquareEntranceComplete, isSquareEntranceComplete}) => (
     <>
       {squareCenters.map((center, index) => (
-        <Square center={center} index={index} key={index} />
+        <Square
+          center={center}
+          index={index}
+          onEntranceComplete={onSquareEntranceComplete}
+          key={index}
+        />
       ))}
+      {isSquareEntranceComplete && <CenterSquare />}
     </>
   ),
 )
